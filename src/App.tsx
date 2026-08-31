20692057c10246fdbf20e8c30190020372a3a592
Will Schwantes <wilschwa@udel.edu>
2026-08-30 23:10:06 -0400

    Added image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import pups from './assets/images/pups.jpg'
 
 function App(): React.JSX.Element {
     return (
@@ -8,12 +9,19 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                <h1>Hello World!</h1>
+                <h1 style={{backgroundColor:"#f00"}}>Hello World!</h1>
                 <br></br>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
                 <p>This is Will Schwantes&apos;s fork!</p>
+
             </p>
+            <img
+                src={pups}
+                alt="My dogs from left to right, Remi, an English Golden Retriever; Leo, a Bernese Mountain Dog; and Lily, a Leonberger."
+                style={{width:"50%",padding:"1em"}}
+            />
+            <figcaption>These are my dogs, Remi, Leo, and Lily.</figcaption>
         </div>
     );
 }
