3841f6a08bd6d0e97a49484862fe531e7728fb28
Will Schwantes <wilschwa@udel.edu>
2026-09-02 16:01:49 -0400

    Added convertYesNo functionality

--- src/functions.ts
+++ src/functions.ts
@@ -47,5 +47,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    let val = null;
+    if (word.toLowerCase() === "yes") {
+        val = true;
+    }
+    if (word.toLowerCase() === "no") {
+        val = false;
+    }
+    return val;
 }
