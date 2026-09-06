87d58fa0413338883f88d98e5d5e36390d5a3945
Will Schwantes <wilschwa@udel.edu>
2026-09-06 11:36:47 -0500

    Implemented 'makeMath' functionality

--- src/arrays.ts
+++ src/arrays.ts
@@ -98,7 +98,7 @@ export function makeMath(addends: number[]): string {
             ),
         ) +
         "=" +
-        String(addends)
+        addends.join("+")
     );
 }
 
@@ -112,5 +112,5 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    return values;
 }
