a64fca61b8395e90f3ab7e3b9efcdb57f6f8de8e
Will Schwantes <wilschwa@udel.edu>
2026-09-06 13:21:27 -0500

    Fixed linting error on line 44

--- src/objects.ts
+++ src/objects.ts
@@ -40,9 +40,8 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    if (question.type === "short_answer_question") { return true; }
-    else if (question.type === "multiple_choice_question") { return Boolean(question.options.find((option: string): boolean => option === answer)); }
-    return false;
+    if (question.type === "multiple_choice_question") { return Boolean(question.options.find((option: string): boolean => option === answer)); }
+    else {return true;}
 }
 
 /**
